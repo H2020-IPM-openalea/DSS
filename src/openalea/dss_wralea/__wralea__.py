1a21cef39b0581ba25e0726aa96ae3e96bdaf30a
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2020-09-10 16:54:17 +0200

    setup module

---
+++ src/openalea/dss_wralea/__wralea__.py
@@ -0,0 +1,21 @@
+
+# This file has been generated at Fri May 31 10:05:45 2013
+
+from openalea.core import *
+
+
+__name__ = 'dss'
+
+__editable__ = True
+__description__ = ''
+__license__ = 'CeCILL-C'
+__url__ = 'http://openalea.gforge.inria.fr'
+__alias__ = []
+__version__ = '0.8.0'
+__authors__ = ''
+__institutes__ = None
+__icon__ = ''
+
+
+__all__ = []
+
